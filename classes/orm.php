3a09e9f87170481d212e294e4eead19ad3277ba8
aonde <equipe@aonde.com>
2011-12-16 11:08:47 -0200

    changes for migration to 3.2

--- classes/orm.php
+++ classes/orm.php
@@ -1,3 +1,23 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class ORM extends Kohana_ORM {}
+class ORM extends Kohana_ORM {
+    
+    
+    	/**
+	 * Creates and returns a new model.
+	 *
+	 * @chainable
+	 * @param   string  $model  Model name
+	 * @param   mixed   $id     Parameter for find()
+	 * @return  ORM
+	 */
+     
+	public static function factory($model, $id = NULL)
+	{
+		// Set class name
+		$model = 'Model_'.ucfirst($model);
+
+		return new $model($id);
+	}
+    
+}
